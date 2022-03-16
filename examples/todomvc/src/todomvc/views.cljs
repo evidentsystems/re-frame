b84d93eb640fc79f3c66d70a6142b776b028632b
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2022-03-16 10:44:12 -0600

    WIP mostly working todomvc example

--- examples/todomvc/src/todomvc/views.cljs
+++ examples/todomvc/src/todomvc/views.cljs
@@ -1,109 +1,116 @@
 (ns todomvc.views
-  (:require [reagent.core  :as reagent]
-            [re-frame.core :refer [subscribe dispatch]]
-            [clojure.string :as str]))
+  (:require [re-frame.core :refer [subscribe dispatch]]
+            [re-frame.reactive :as reactive]
+            [clojure.string :as str]
+            ["solid-js" :refer [Show For]]))
 
-
-(defn todo-input [{:keys [title on-save on-stop]}]
-  (let [val  (reagent/atom title)
+(defn todo-input
+  [{:keys [title onSave onStop] :as props}]
+  (js/console.log ::todo-input props)
+  (let [val  (reactive/atom title)
         stop #(do (reset! val "")
-                  (when on-stop (on-stop)))
+                  (when onStop (onStop %)))
         save #(let [v (-> @val str str/trim)]
-                (on-save v)
-                (stop))]
-    (fn [props]
-      [:input (merge (dissoc props :on-save :on-stop :title)
-                     {:type        "text"
-                      :value       @val
-                      :auto-focus  true
-                      :on-blur     save
-                      :on-change   #(reset! val (-> % .-target .-value))
-                      :on-key-down #(case (.-which %)
-                                      13 (save)
-                                      27 (stop)
-                                      nil)})])))
-
+                (js/console.log ::save v)
+                (onSave v)
+                (stop %))]
+    [:input (merge (dissoc props :title :onSave :onStop)
+                   {:type      "text"
+                    #_#_:value     #(val)
+                    :autoFocus true
+                    #_#_:onBlur    save
+                    #_#_:onChange  #(reset! val (-> % .-target .-value))
+                    #_#_:onKeyDown #(case (.-which %)
+                                  13 (save %)
+                                  27 (stop %)
+                                  nil)})]))
 
 (defn todo-item
-  []
-  (let [editing (reagent/atom false)]
-    (fn [{:keys [id done title]}]
-      [:li {:class (str (when done "completed ")
-                        (when @editing "editing"))}
-        [:div.view
-          [:input.toggle
-            {:type "checkbox"
-             :checked done
-             :on-change #(dispatch [:toggle-done id])}]
-          [:label
-            {:on-double-click #(reset! editing true)}
-            title]
-          [:button.destroy
-            {:on-click #(dispatch [:delete-todo id])}]]
-        (when @editing
-          [todo-input
-            {:class "edit"
-             :title title
-             :on-save #(if (seq %)
-                          (dispatch [:save id %])
-                          (dispatch [:delete-todo id]))
-             :on-stop #(reset! editing false)}])])))
-
+  [{:keys [id done title] :as item}]
+  (js/console.log ::todo-item item :type (type item))
+  (let [editing (reactive/atom false)]
+    [:li {:class #(str (when done "completed ")
+                       (when @editing "editing"))}
+     [:div.view
+      [:input.toggle
+       {:type     "checkbox"
+        :checked  done
+        :onchange (fn [_e] (dispatch [:toggle-done id]))}]
+      [:label
+       {:ondblclick (fn [_e] (reset! editing true))}
+       title]
+      [:button.destroy
+       {:onclick (fn [_e] (dispatch [:delete-todo id]))}]]
+     [:> Show {:when #(editing)}
+      [todo-input
+       {:class  "edit"
+        :title  title
+        :onSave #(if (seq %)
+                   (dispatch [:save id %])
+                   (dispatch [:delete-todo id]))
+        :onStop (fn [_e] (reset! editing false))}]]]))
 
 (defn task-list
   []
-  (let [visible-todos @(subscribe [:visible-todos])
-        all-complete? @(subscribe [:all-complete?])]
-      [:section#main
-        [:input#toggle-all
-          {:type "checkbox"
-           :checked all-complete?
-           :on-change #(dispatch [:complete-all-toggle])}]
-        [:label
-          {:for "toggle-all"}
-          "Mark all as complete"]
-        [:ul#todo-list
-          (for [todo  visible-todos]
-            ^{:key (:id todo)} [todo-item todo])]]))
-
+  (let [visible-todos (subscribe [:visible-todos])
+        all-complete? (subscribe [:all-complete?])]
+    [:section#main
+     [:input#toggle-all
+      {:type "checkbox"
+       :checked @all-complete?
+       :onChange #(dispatch [:complete-all-toggle])}]
+     [:label {:for "toggle-all"} "Mark all as complete"]
+     [:ul#todo-list
+      [:> For {:each (fn [e] (js/console.log ::task-list e) (into-array @visible-todos))}
+       [todo-item]]]]))
 
 (defn footer-controls
   []
-  (let [[active done] @(subscribe [:footer-counts])
-        showing       @(subscribe [:showing])
-        a-fn          (fn [filter-kw txt]
-                        [:a {:class (when (= filter-kw showing) "selected")
-                             :href (str "#/" (name filter-kw))} txt])]
+  (let [counts     (subscribe [:footer-counts])
+        active     #(first @counts)
+        plural?    #(case (active) 1 "item" "items")
+        some-done? #(pos? (last @counts))
+        showing    (subscribe [:showing])
+        a-fn       (fn [filter-kw txt]
+                     [:a {:class #(when (= filter-kw @showing) "selected")
+                          :href  (str "#/" (name filter-kw))} txt])]
     [:footer#footer
      [:span#todo-count
-      [:strong active] " " (case active 1 "item" "items") " left"]
+      [:strong active] " " plural? " left"]
      [:ul#filters
       [:li (a-fn :all    "All")]
       [:li (a-fn :active "Active")]
       [:li (a-fn :done   "Completed")]]
-     (when (pos? done)
-       [:button#clear-completed {:on-click #(dispatch [:clear-completed])}
-        "Clear completed"])]))
-
+     [:> Show {:when some-done?}
+      [:button#clear-completed
+       {:onclick (fn [_e] (dispatch [:clear-completed]))}
+       "Clear completed"]]]))
 
 (defn task-entry
   []
   [:header#header
-    [:h1 "todos"]
-    [todo-input
-      {:id "new-todo"
-       :placeholder "What needs to be done?"
-       :on-save #(when (seq %)
-                    (dispatch [:add-todo %]))}]])
-
+   [:h1 "todos"]
+   [todo-input
+    {:id          "new-todo"
+     :placeholder "What needs to be done?"
+     :onSave      #(when (seq %)
+                     (dispatch [:add-todo %]))}]])
 
 (defn todo-app
   []
-  [:<>
-   [:section#todoapp
-    [task-entry]
-    (when (seq @(subscribe [:todos]))
-      [task-list])
-    [footer-controls]]
-   [:footer#info
-    [:p "Double-click to edit a todo"]]])
+  (let [todos (subscribe [:todos])]
+    [:<>
+     [:section#todoapp
+      [task-entry]
+      [:> Show {:when #(seq @todos)}
+       [task-list]]
+      [footer-controls]]
+     [:footer#info
+      [:p "Double-click to edit a todo"]]]))
+
+(comment
+
+  (re-frame.views/hyperscript (todo-app))
+
+  ;;
+  )
